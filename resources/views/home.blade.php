d723a7d25a597c0ecdf05a5059e0d38d6d605bc8
PabloProgramador77 <pabloprogramador77@gmail.com>
2023-11-06 17:16:49 -0600

    Configuración Inicial de Proyecto con Registro y Login de IPES

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('adminlte::page')
+@section('title', 'SIEBDATA')
+@section('plugins.Datatables', true)
+@section('plugins.Select2', true)
+@section('plugins.JQuery', true)
+@section('plugins.Sweetalert2', true)
+@section('content_header')
+    <h1>¡Bienvenido a SIEBDATA!</h1>
+@stop
