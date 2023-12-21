c2c7884a9543529dd2d73d87194efcb4a5615110
PabloProgramador77 <pabloprogramador77@gmail.com>
2023-12-21 09:44:29 -0600

    Creación del Botón de Cambio de Modalidad

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,7 +2,8 @@
 @section('contenido')
     <div class="container-fluid col-md-12 bg-white p-2 rounded">
         
-        <p class="fs-3 fw-bold text-center bg-info p-2 my-4 rounded shadow">Resumen Institucional</p>
+        <p class="fs-3 fw-bold text-center bg-light border p-2 my-4 rounded shadow">Resumen Institucional</p>
+        <x-adminlte-input-switch id="modo" name="modo" data-on-text="Modo Certificado" data-off-text="Modo Título"></x-adminlte-input-switch>
         <div class="container-fluid row">
             
             <div class="col-md-4">
@@ -25,4 +26,4 @@
         
     </div>
 
-@stop
+@endsection
