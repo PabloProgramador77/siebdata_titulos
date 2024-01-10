91135aed17d6be78a566c82a3305ba42cb836968
PabloProgramador77 <pabloprogramador77@gmail.com>
2024-01-10 12:44:05 -0600

    Carga de Datos a XML Recien Creado

--- app/Http/Controllers/ArchivoController.php
+++ app/Http/Controllers/ArchivoController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Archivo;
+use App\Models\Responsable;
+use App\Models\User;
+use App\Models\Expedicion;
 use Illuminate\Http\Request;
 use App\Http\Requests\Archivo\Create;
 use App\Http\Requests\Archivo\Read;
@@ -42,6 +45,98 @@ class ArchivoController extends Controller
             $xml->setIndent( true );
 
             if( file_exists( public_path('xml/'.$request->folio.'.xml') ) ){
+
+                $xml->startDocument('1.0', 'utf-8');
+                    $xml->startElement('TituloElectronico');
+                        $xml->writeAttribute('version', '1.0');
+                        $xml->writeAttribute('folioControl', $request->folio);
+                        $xml->writeAttribute('xmlns:xs', 'http://www.w3.org/2001/XMLSchema-instance');
+                        $xml->writeAttribute('xmlns', 'https://www.siged.sep.gob.mx/titulos/');
+                        $xml->writeAttribute('xs:schemaLocation', 'https://siged.sep.gob.mx/titulos/');
+
+                        $responsables = Responsable::all();
+
+                        $xml->startElement('FirmaResponsables');
+                                
+                            foreach( $responsables as $responsable ){
+
+                                $xml->startElement('FirmaResponsable');
+                                    $xml->writeAttribute('nombre', mb_strtoupper( $responsable->nombre, 'utf-8' ));
+                                    $xml->writeAttribute('primerApellido', mb_strtoupper( $responsable->primerApellido, 'utf-8' ));
+                                    
+                                    if( $responsable->segundoApellido != '' ){
+                                        $xml->writeAttribute('segundoApellido', mb_strtoupper( $responsable->segundoApellido, 'utf-8' ));
+                                    }
+
+                                    $xml->writeAttribute('curp', mb_strtoupper( $responsable->curp, 'utf-8' ));
+                                    $xml->writeAttribute('idCargo', $responsable->cargo->id);
+                                    $xml->writeAttribute('cargo', mb_strtoupper( $responsable->cargo->descripcion, 'utf-8' ));
+                                    
+                                    if( $responsable->titulo != '' ){
+                                        $xml->writeAttribute('abrTitulo', mb_strtoupper( $responsable->titulo, 'utf-8' ));
+                                    }
+
+                                    $xml->writeAttribute('sello', '');
+                                    $xml->writeAttribute('certificadoResponsable', $responsable->certificado->noCertificado);
+                                    $xml->writeAttribute('noCertificadoResponsable', $responsable->certificado->serial);
+                                $xml->endElement();
+                            }
+                            
+                        $xml->endElement();
+                        
+                        $ipes = User::find( auth()->user()->id );
+
+                        $xml->startElement('Institucion');
+                            $xml->writeAttribute('cveInstitucion', $ipes->clave);
+                            $xml->writeAttribute('nombreInstitucion', mb_strtoupper( $ipes->name, 'utf-8' ));
+                        $xml->endElement();
+
+                        $expedicion = Expedicion::find( $request->idExpedicion );
+
+                        $xml->startElement('Carrera');
+                            $xml->writeAttribute('cveCarrera', $expedicion->alumno->carrera->clave);
+                            $xml->writeAttribute('nombreCarrera', mb_strtoupper( $expedicion->alumno->carrera->nombre, 'utf-8' ));
+                            $xml->writeAttribute('fechaInicio', $expedicion->alumno->fechaInicio);
+                            $xml->writeAttribute('fechaTerminacion', $expedicion->alumno->fechaTermino);
+                            $xml->writeAttribute('idAutorizacionReconocimiento', $expedicion->alumno->carrera->autoridad->idAutoridad);
+                            $xml->writeAttribute('autorizacionReconocimiento', mb_strtoupper( $expedicion->alumno->carrera->autoridad->nombre ));
+                            $xml->writeAttribute('numeroRvoe', $expedicion->alumno->carrera->rvoe);
+                        $xml->endElement();
+
+                        $xml->startElement('Profesionista');
+                            $xml->writeAttribute('correoElectronico', $expedicion->alumno->email);
+
+                            if( $expedicion->alumno->segundoApellido != '' ){
+                                $xml->writeAttribute('segundoApellido', mb_strtoupper( $expedicion->alumno->segundoApellido, 'utf-8' ));
+                            }
+                            
+                            $xml->writeAttribute('primerApellido', mb_strtoupper( $expedicion->alumno->primerApellido, 'utf-8' ));
+                            $xml->writeAttribute('nombre', mb_strtoupper( $expedicion->alumno->nombre, 'utf-8' ));
+                            $xml->writeAttribute('curp', $expedicion->alumno->curp);
+                        $xml->endElement();
+
+                        $xml->startElement('Expedicion');
+                            $xml->writeAttribute('fechaExpedicion', $expedicion->created_at->format('Y-m-d'));
+                            $xml->writeAttribute('idModalidadTitulacion', $expedicion->titulacion->idTitulacion);
+                            $xml->writeAttribute('modalidadTitulacion', mb_strtoupper( $expedicion->titulacion->nombre, 'utf-8' ));
+
+                            if( $expedicion->fechaExamen != '' ){
+                                $xml->writeAttribute('fechaExamenProfesional', $expedicion->fechaExamen);
+                            }
+
+                            if( $expedicion->fechaExencion != '' ){
+                                $xml->writeAttribute('fechaExencionExamenProfesional', $expedicion->fechaExencion);
+                            }
+
+                            $xml->writeAttribute('cumplioServicioSocial', $expedicion->servicioSocial);
+                            $xml->writeAttribute('idFundamentoLegalServicioSocial', $expedicion->fundamento->idFundamento);
+                            $xml->writeAttribute('fundamentoLegalServicioSocial', mb_strtoupper( $expedicion->fundamento->nombre, 'utf-8' ));
+                            $xml->writeAttribute('idEntidadFederativa', $expedicion->entidad->idEntidad);
+                            $xml->writeAttribute('entidadFederativa', mb_strtoupper( $expedicion->entidad->nombre, 'utf-8' ));
+                        $xml->endElement();
+
+                    $xml->endElement();
+                $xml->fullEndElement();
 
                 return true;
 
